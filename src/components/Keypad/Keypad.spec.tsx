d6f82cfc3323dcca8bb254ad5b762f5662a6e75c
Rommel Rico <me@rommelrico.com>
2023-01-16 12:26:21 -0800

    :white_check_mark: Keyboard: Rendering values of numbers

--- src/components/Keypad/Keypad.spec.tsx
+++ src/components/Keypad/Keypad.spec.tsx
@@ -19,7 +19,7 @@ describe('Keypad', () => {
   });
 
   it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('div').length).toEqual(2);
   });
 
   it('renders the values of numbers', () => {
--- src/components/Keypad/Keypad.tsx
+++ src/components/Keypad/Keypad.tsx
@@ -10,7 +10,16 @@ type Props = {
 
 const Keypad = (props: Props) => {
   const { callOperator, numbers, operators, setOperator, updateDisplay } = props;
-  return <div className="keypad-container"/>;
+
+  const numberKeys = numbers.map(number => <p key={number}>{number}</p>);
+
+  return (
+    <div className="keypad-container">
+      <div className="numbers-container">
+        {numberKeys}
+      </div>
+    </div>
+  );
 }
 
 export default Keypad;
