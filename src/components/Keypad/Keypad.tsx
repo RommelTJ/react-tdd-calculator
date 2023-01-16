603777e745ab4169fd8804a18ed219b3a3d93f13
Rommel Rico <me@rommelrico.com>
2023-01-16 12:28:22 -0800

    :white_check_mark: Keyboard: Renders the values of operators

--- src/components/Keypad/Keypad.tsx
+++ src/components/Keypad/Keypad.tsx
@@ -12,12 +12,12 @@ const Keypad = (props: Props) => {
   const { callOperator, numbers, operators, setOperator, updateDisplay } = props;
 
   const numberKeys = numbers.map(number => <p key={number}>{number}</p>);
+  const operatorKeys = operators.map(operator => <p key={operator}>{operator}</p>);
 
   return (
     <div className="keypad-container">
-      <div className="numbers-container">
-        {numberKeys}
-      </div>
+      <div className="numbers-container">{numberKeys}</div>
+      <div className="operators-container">{operatorKeys}</div>
     </div>
   );
 }
