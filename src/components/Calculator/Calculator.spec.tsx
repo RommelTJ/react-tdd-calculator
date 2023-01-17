bc3d19001d70e8d48e191a4f6f84f32eb083765b
Rommel Rico <me@rommelrico.com>
2023-01-17 07:09:04 -0800

    :test_tube: Part2: Click event tests

--- src/components/Calculator/Calculator.spec.tsx
+++ src/components/Calculator/Calculator.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, RenderResult, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
 
 import Calculator from './Calculator';
 import display from "../Display/Display";
@@ -21,6 +22,30 @@ describe('Calculator', () => {
     expect(displayComponent).toBeTruthy();
     const keypadComponent = screen.getByTestId('keypad');
     expect(keypadComponent).toBeTruthy();
+  });
+
+  it('calls updateDisplay when a number key is clicked', async () => {
+    const logSpy = jest.spyOn(console, 'log');
+    expect(logSpy).toHaveBeenCalledTimes(0);
+    const numberKeys = wrapper.getAllByRole("number-key");
+    await userEvent.click(numberKeys[0]);
+    expect(logSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls setOperator when an operator key is clicked', async () => {
+    const logSpy = jest.spyOn(console, 'log');
+    expect(logSpy).toHaveBeenCalledTimes(0);
+    const operatorKeys = wrapper.getAllByRole("operator-key");
+    await userEvent.click(operatorKeys[0]);
+    expect(logSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls callOperator when the submit key is clicked', async () => {
+    const logSpy = jest.spyOn(console, 'log');
+    expect(logSpy).toHaveBeenCalledTimes(0);
+    const submitKey = wrapper.getByRole("submit-key");
+    await userEvent.click(submitKey);
+    expect(logSpy).toHaveBeenCalledTimes(1);
   });
 
 });
